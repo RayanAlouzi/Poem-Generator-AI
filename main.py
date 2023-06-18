74cf5c8df5082f7d9f125e39acc5e8c25afd8816
Rayan Alouzi <rayanbassam3@gmail.com>
2023-06-17 23:48:21 -0600

    creating main libraries

---
+++ main.py
@@ -0,0 +1,6 @@
+import random 
+import numpy as np 
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
